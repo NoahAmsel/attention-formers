f5bfd3f669997e35e9c5816777cd802e4d3144dd
Noah Amsel <noah.amsel@nyu.edu>
2023-09-25 00:56:05 -0400

    MultiheadSelfAttention

--- models.py
+++ models.py
@@ -1,8 +1,6 @@
 from math import sqrt
-from typing import Any, Optional
 
 import lightning.pytorch as pl
-from lightning.pytorch.utilities.types import STEP_OUTPUT
 import torch
 
 
@@ -34,29 +32,66 @@ class LRSelfAttentionHead(torch.nn.Module):
         return torch.einsum("btu,bud,de->bte", attn_matrix, x, self.VO)
 
     @classmethod
-    def farthest_init(cls, dim, device=None, dtype=None):
+    def farthest_init(cls, dim, temperature=10000, device=None, dtype=None):
         model = cls(dim, dim, device=device, dtype=dtype)
-        model.Q.data = -10000 * torch.eye(model.dim, device=device, dtype=dtype)
+        model.Q.data = -temperature * torch.eye(model.dim, device=device, dtype=dtype)
         model.K.data = torch.eye(model.dim, device=device, dtype=dtype)
         model.VO.data = torch.eye(model.dim, device=device, dtype=dtype)
         return model
 
 
-class MergedLinearFormer(torch.nn.Module):
-    def __init__(self, d_model, batch_first=True, device=None, dtype=None):
+class MultiheadSelfAttention(torch.nn.Module):
+    def __init__(self, dim, rank, nheads, device=None, dtype=None):
         super().__init__()
-        self.d_model = d_model
-        self.QK = torch.nn.Parameter(torch.empty((self.d_model, self.d_model), device=device, dtype=dtype))
-        self.VO = torch.nn.Parameter(torch.empty((self.d_model, self.d_model), device=device, dtype=dtype))
+        self.dim = dim
+        self.Q = torch.nn.Parameter(torch.empty((nheads, rank, dim), device=device, dtype=dtype))
+        self.K = torch.nn.Parameter(torch.empty((nheads, rank, dim), device=device, dtype=dtype))
+        self.VO = torch.nn.Parameter(torch.empty((nheads, dim, dim), device=device, dtype=dtype))
+        self._reset_parameters()
+
+
+    def _reset_parameters(self):
+        torch.nn.init.xavier_uniform_(self.Q)
+        torch.nn.init.xavier_uniform_(self.K)
+        torch.nn.init.xavier_uniform_(self.VO)        
+
+    def assemble_QK(self, head_ix):
+        return self.Q.data[head_ix, :, :].T @ self.K.data[head_ix, :, :]
+
+    def assemble_VO(self, head_ix):
+        return self.VO.data[head_ix, :, :]
+
+    def forward(self, x):
+        # x is batch_size, ntokens, dim
+        # self.Q and self.K are each rank by dim
+        # attn_matrix is batch_size, ntokens, ntokens
+        attn_matrix = torch.nn.Softmax(dim=3)(torch.einsum("btd,hrd,hre,bue->bthu", x, self.Q, self.K, x) / sqrt(self.dim))
+        return torch.einsum("bthu,bud,hde->bte", attn_matrix, x, self.VO)
+
+    @classmethod
+    def farthest_init(cls, dim, temperature=10000, device=None, dtype=None):
+        model = cls(dim=dim, rank=dim, nheads=1, device=device, dtype=dtype)
+        model.Q.data[0,:,:] = -temperature * torch.eye(model.dim, device=device, dtype=dtype)
+        model.K.data[0,:,:] = torch.eye(model.dim, device=device, dtype=dtype)
+        model.VO.data[0,:,:] = torch.eye(model.dim, device=device, dtype=dtype)
+        return model
+
+
+class MergedLinearFormer(torch.nn.Module):
+    def __init__(self, dim, batch_first=True, device=None, dtype=None):
+        super().__init__()
+        self.dim = dim
+        self.QK = torch.nn.Parameter(torch.empty((self.dim, self.dim), device=device, dtype=dtype))
+        self.VO = torch.nn.Parameter(torch.empty((self.dim, self.dim), device=device, dtype=dtype))
         self._reset_parameters()
 
     def _reset_parameters(self):
         torch.nn.init.xavier_uniform_(self.QK)
         torch.nn.init.xavier_uniform_(self.VO)
 
-    def perturb_parameters(self, scale=1.):
-        self.QK.data += scale * torch.randn_like(self.QK.data)
-        self.VO.data += scale * torch.randn_like(self.VO.data)
+    # def perturb_parameters(self, scale=1.):
+    #     self.QK.data += scale * torch.randn_like(self.QK.data)
+    #     self.VO.data += scale * torch.randn_like(self.VO.data)
 
     def assemble_QK(self):
         return self.QK.data
@@ -67,28 +102,27 @@ class MergedLinearFormer(torch.nn.Module):
     def forward(self, x):
         # x is batch_size, ntokens, dim
         # attn_matrix is batch_size, ntokens, ntokens
-        attn_matrix = torch.nn.Softmax(dim=2)(torch.einsum("btd,de,bue->btu", x, self.QK, x) / sqrt(self.d_model))
+        attn_matrix = torch.nn.Softmax(dim=2)(torch.einsum("btd,de,bue->btu", x, self.QK, x) / sqrt(self.dim))
         return torch.einsum("btu,bud,de->bte", attn_matrix, x, self.VO)
 
     @classmethod
     def farthest_init(cls, d_model, batch_first=True, device=None, dtype=None):
         model = cls(d_model, batch_first=batch_first, device=device, dtype=dtype)
-        model.QK.data = -10000 * torch.eye(model.d_model, device=device, dtype=dtype)
-        model.VO.data = torch.eye(model.d_model, device=device, dtype=dtype)
+        model.QK.data = -10000 * torch.eye(model.dim, device=device, dtype=dtype)
+        model.VO.data = torch.eye(model.dim, device=device, dtype=dtype)
         return model
 
-    @classmethod
-    def extract_attn(cls, my_layer):
-        dim = my_layer.self_attn.out_proj.weight.data.shape[0]
-        merged = cls(dim)
-        merged.QK.data = my_layer.self_attn.in_proj_weight.data[:dim, :].T \
-            @ my_layer.self_attn.in_proj_weight.data[dim:(2*dim), :]
-        merged.VO.data = my_layer.self_attn.in_proj_weight.data[(
-            2*dim):, :].T @ my_layer.self_attn.out_proj.weight.data.T @ my_layer.linear.weight.T
-        return merged
 
-def angle_deg(a, b):
-    return torch.acos(torch.inner(a, b) / (torch.norm(a) * torch.norm(b))) * 180 / torch.pi
+def compare_to_identity(matrix):
+    dim = matrix.shape[0]
+    assert dim == matrix.shape[1]
+    # normalize to scale of I. l1 norm of diagonal should be dim to match that of I
+    scale = torch.norm(matrix.diagonal(), p=1) / dim
+    scaled_matrix = matrix / scale
+    max_diag_error = torch.norm(scaled_matrix.diagonal() - 1, p=torch.inf)
+    scaled_matrix.fill_diagonal_(0)
+    max_off_diag_error = torch.norm(scaled_matrix, p=torch.inf)
+    return scale, max_diag_error, max_off_diag_error
 
 class LitSequenceRegression(pl.LightningModule):
     def __init__(self, model, lr):
@@ -102,22 +136,18 @@ class LitSequenceRegression(pl.LightningModule):
         loss = torch.nn.functional.mse_loss(y_hat, y)
         # Logging to TensorBoard (if installed) by default
         self.log("train_loss", loss, on_step=False, on_epoch=True, prog_bar=True, logger=True)
-        # self.log("angle", (180 / torch.pi) * torch.acos(1 - loss/2), on_step=False, on_epoch=True, prog_bar=True, logger=True)
         return loss
-    
+
     def on_train_epoch_end(self):
-        dim = self.model.assemble_QK().shape[0]
-        # normalize to scale of I. l1 norm of diagonal should be dim to match I
-        QK = self.model.assemble_QK()
-        QK_scale = torch.norm(QK.diagonal(), p=1) / dim
-        scaled_QK = QK / QK_scale
-        VO = self.model.assemble_VO()
-        VO_scale = torch.norm(VO.diagonal(), p=1) / dim
-        scaled_VO = VO / VO_scale
-        self.log("QK error", torch.norm(scaled_QK + torch.eye(dim, device=scaled_QK.device), p=torch.inf), prog_bar=True, logger=True)
+        # QK should be like -I, not I
+        QK_scale, QK_diag_error, QK_off_diag_error = compare_to_identity(-self.model.assemble_QK(0))
+        VO_scale, VO_diag_error, VO_off_diag_error = compare_to_identity(self.model.assemble_VO(0))
         self.log("QK scale", QK_scale, prog_bar=True, logger=True)
+        self.log("QK diag error", QK_diag_error, prog_bar=True, logger=True)
+        self.log("QK off diag error", QK_off_diag_error, prog_bar=True, logger=True)
         self.log("VO scale", VO_scale, prog_bar=True, logger=True)
-        self.log("VO error", torch.norm(scaled_VO - torch.eye(dim, device=scaled_VO.device), p=torch.inf), prog_bar=True, logger=True)
+        self.log("VO diag error", VO_diag_error, prog_bar=True, logger=True)
+        self.log("VO off diag error", VO_off_diag_error, prog_bar=True, logger=True)
 
     def test_step(self, batch, batch_ix):
         x, y = batch
@@ -145,9 +175,9 @@ if __name__ == "__main__":
     ntokens = 100
     dim = 100
     batch_size = 64
-    lit_model = LitSequenceRegression(MergedLinearFormer(dim), lr=1e-1)
-    lit_model = LitSequenceRegression(LRSelfAttentionHead(dim, int(dim * .9)), lr=1e-2)
-    # lit_model = LitSequenceRegression(MergedLinearFormer.farthest_init(dim))
+    # lit_model = LitSequenceRegression(MergedLinearFormer(dim), lr=1e-1)
+    # lit_model = LitSequenceRegression(MultiheadSelfAttention(dim=dim, rank=dim, nheads=3), lr=1e-2)
+    lit_model = LitSequenceRegression(MultiheadSelfAttention.farthest_init(dim, temperature=1e9), lr=0)
     # lit_model.model.perturb_parameters(0.3)
     data = torch.utils.data.DataLoader(FarthestPointDataset(ntokens, dim), batch_size=batch_size, num_workers=4)
     trainer = pl.Trainer(limit_train_batches=100, limit_test_batches=100, max_epochs=200, callbacks=[pl.callbacks.LearningRateMonitor(logging_interval='epoch')])
